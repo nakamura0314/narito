3c8c47d0113e1266ba4537ce788233bc85f8218f
中村渉帆 <takaho@Takaho-Nakamura.local>
2021-01-26 19:42:31 +0900

    不備なし

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DayCreateForm
 from .models import Day
 
@@ -13,6 +13,25 @@ def index(request):
 def add(request):
     # 送信内容を基にフォームを作る。POSTじゃなければ、空のフォーム
     form = DayCreateForm(request.POST or None)
+
+    # method=POST、つまり送信ボタン押下時、入力内容が問題なければ
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+
+    # 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
+    context = {
+        'form': form
+    }
+    return render(request, 'diary/day_form.html', context)
+
+
+def update(request, pk):
+    # urlのpkを基に、Dayを取得
+    day = get_object_or_404(Day, pk=pk)
+
+    # フォームに、取得したDayを紐づける
+    form = DayCreateForm(request.POST or None, instance=day)
 
     # method=POST、つまり送信ボタン押下時、入力内容が問題なければ
     if request.method == 'POST' and form.is_valid():
